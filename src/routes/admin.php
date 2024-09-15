ba01f085e18486662bf7dc786a837a77e5dcbbf5
Arshidin <osm.arshidin@gmail.com>
2024-09-15 20:09:46 +0500

    move in Admin folder

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Admin\Auth\LoginController;
+use App\Http\Controllers\Admin\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [LoginController::class, 'showForm'])->name('loginForm');
